4859f15894e6c7acc2578d4cfbf37f9201843a43
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2025-10-09 00:17:10 +0100

    feat: remove Header and Footer components along with their styles

--- src/components/Footer.jsx
+++
@@ -1,5 +0,0 @@
-import styles from "./Footer.module.css";
-
-export function Footer() {
-  return <div className={styles.footer}>© 2025 My Next.js App</div>;
-}
--- src/components/Header.jsx
+++
@@ -1,5 +0,0 @@
-import styles from "./Header.module.css";
-
-export function Header() {
-  return <div className={styles.header}>My Next.js App</div>;
-}
