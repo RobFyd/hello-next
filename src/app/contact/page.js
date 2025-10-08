1e37737c1596c4df39ce598b843ff7f7369539ec
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2025-10-09 00:36:51 +0100

    feat: remove Header and Footer components from Contact and Home pages

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -1,12 +1,7 @@
-import { Footer } from "../components/Footer";
-import { Header } from "../components/Header";
-
 export default function Contact() {
   return (
     <>
-      <Header />
       <h1>Contact Us</h1>
-      <Footer />
     </>
   );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,8 @@
-import { Footer } from "./components/Footer";
-import { Header } from "./components/Header";
-
 export default function Home() {
   return (
     <main>
-      <Header />
       <h1>Welcome to My Next.js App!</h1>
       <p>This is the home page of the application.</p>
-      <Footer />
     </main>
   );
 }
