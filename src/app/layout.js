afb67d44f7e8cb4c760e0ff226fb7d8ac45bf6ef
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2025-10-09 00:37:02 +0100

    feat: integrate Header and Footer components into RootLayout

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,6 @@
+import { Footer } from "./components/Footer";
+import { Header } from "./components/Header";
+
 export const metadata = {
   title: "My Next.js App",
   description: "A simple Next.js application",
@@ -6,7 +9,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <Header />
+        {children}
+        <Footer />
+      </body>
     </html>
   );
 }
