af223c5af168717af81755e63ada084e17a911b0
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2025-10-10 23:58:06 +0100

    feat: enhance DynamicRoute to display gender-specific labels

--- src/app/[gender]/page.js
+++ src/app/[gender]/page.js
@@ -1,3 +1,9 @@
+const PARAM_TO_WORD = {
+  men: "Men💪🏼",
+  women: "Women👄",
+  kids: "Kids🪁",
+};
+
 export default function DynamicRoute({ params }) {
-  return <h2>Dynamic Route page for: {params.gender}.</h2>;
+  return <h2>Dynamic Route page for: {PARAM_TO_WORD[params.gender]}.</h2>;
 }
