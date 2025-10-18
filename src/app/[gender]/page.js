6b8928f23a1ea4d46cdcea727538e3176d9b7fab
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2025-10-18 23:58:30 +0100

    feat: update emoji for 'others' label in DynamicRoute

--- src/app/[gender]/page.js
+++ src/app/[gender]/page.js
@@ -4,11 +4,22 @@ const PARAM_TO_WORD_WITH_EMOJI = {
   kids: "Kids🪁",
   unisex: "Unisex👕",
   animals: "Animals🐶",
-  others: "Others🎲",
+  others: "Others❓",
 };
 
 export default function DynamicRoute({ params }) {
   return (
     <h2>Dynamic Route page for: {PARAM_TO_WORD_WITH_EMOJI[params.gender]}.</h2>
   );
+}
+
+export async function generateStaticParams() {
+  return [
+    { gender: "men" },
+    { gender: "women" },
+    { gender: "kids" },
+    { gender: "unisex" },
+    { gender: "animals" },
+    { gender: "others" },
+  ];
 }
