2d777dcb236c1e238d898d925b1bdbcfa9c0eddc
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2025-10-22 00:18:36 +0100

    feat: display selected language in LanguageSelector component

--- src/app/components/LanguageSelector.jsx
+++ src/app/components/LanguageSelector.jsx
@@ -1,10 +1,19 @@
+"use client";
+import { useState } from "react";
 import styles from "./LanguageSelector.module.css";
 
 export function LanguageSelector() {
+  const [selectedOption, setSelectedOption] = useState();
   return (
-    <select className={styles.languageSelector}>
-      <option value="EN">English</option>
-      <option value="PL">Polish</option>
-    </select>
+    <>
+      <p>Selected Language: {selectedOption}</p>
+      <select
+        className={styles.languageSelector}
+        onChange={(e) => setSelectedOption(e.target.value)}
+      >
+        <option value="EN">English</option>
+        <option value="PL">Polish</option>
+      </select>
+    </>
   );
 }
