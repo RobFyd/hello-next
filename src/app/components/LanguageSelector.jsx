9d0b1e5ac2881b0e67a3f77ce5a1f95ee3300a4e
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2025-10-22 00:26:10 +0100

    feat: set default selected language to English in LanguageSelector component

--- src/app/components/LanguageSelector.jsx
+++ src/app/components/LanguageSelector.jsx
@@ -1,14 +1,15 @@
-"use client";
+"use client"; // Enable client-side rendering
 import { useState } from "react";
 import styles from "./LanguageSelector.module.css";
 
 export function LanguageSelector() {
-  const [selectedOption, setSelectedOption] = useState();
+  const [selectedOption, setSelectedOption] = useState("EN");
   return (
     <>
       <p>Selected Language: {selectedOption}</p>
       <select
         className={styles.languageSelector}
+        value={selectedOption}
         onChange={(e) => setSelectedOption(e.target.value)}
       >
         <option value="EN">English</option>
