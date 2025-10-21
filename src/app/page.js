f41b4106b84c9cb9bb6fb9886cf1a301f0f9ec27
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2025-10-22 00:07:26 +0100

    feat: add LanguageSelector component to Home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,11 @@
+import { LanguageSelector } from "./components/LanguageSelector";
+
 export default function Home() {
   return (
     <main>
       <h1>Welcome to My Next.js App!</h1>
       <p>This is the home page of the application.</p>
+      <LanguageSelector />
     </main>
   );
 }
