025b0640d5acb7e61c34998795b8eca4ddaaa511
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2025-10-25 23:54:10 +0100

    fix: correct import path for summer sale image

--- src/app/photo/page.js
+++ src/app/photo/page.js
@@ -1,4 +1,4 @@
-import summersale from "/public/images/summersale.jpg";
+import summersale from "/public/summersale.jpg";
 
 export default function Photo() {
   return <img src={summersale.src} />;
