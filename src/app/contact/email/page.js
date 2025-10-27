ea05cc2347973438a67ff87f6f3e78bcdd21a851
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2025-10-27 23:56:13 +0000

    fix: update Email component to include additional contact information

--- src/app/contact/email/page.js
+++ src/app/contact/email/page.js
@@ -1,3 +1,9 @@
 export default function Email() {
-  return <h2>Email: contact@example.com</h2>;
+  return (
+    <>
+      <h2>Email: contact@example.com</h2>
+      <p>Please feel free to reach out!</p>
+      <br />
+    </>
+  );
 }
