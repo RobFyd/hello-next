11c5a6b3f33e3d79aa4c06d5d2a432713ec00039
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2025-10-28 00:42:03 +0000

    fix: apply Roboto font styling to the body in RootLayout

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,14 @@
 import { Footer } from "./components/Footer";
 import { Header } from "./components/Header";
 
+import { Roboto } from "next/font/google";
+
+const roboto = Roboto({
+  style: ["normal", "italic"],
+  subsets: ["latin"],
+  weight: ["400", "700"],
+});
+
 export const metadata = {
   title: "My Next.js App",
   description: "A simple Next.js application",
@@ -9,7 +17,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
+      <body className={roboto.className}>
         <Header />
         {children}
         <Footer />
