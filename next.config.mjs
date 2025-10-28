70499c9272b17f48f605590bd3876dc2485236b1
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2025-10-28 00:29:30 +0000

    fix: restore output configuration and re-enable image optimization in nextConfig

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // output: "export",
+  output: "export",
+  images: {
+    unoptimized: true, // <- to wyłącza optymalizację obrazów, żeby <Image /> działał w eksporcie
+  },
 };
 
 export default nextConfig;
